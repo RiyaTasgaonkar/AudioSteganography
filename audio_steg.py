ac83d8d697c8685acaad1e22cfa33f7a56869497
RiyaTasgaonkar <riyatasgaonkar@gmail.com>
2021-02-22 17:08:19 +0530

    updated app

--- audio_steg.py
+++ audio_steg.py
@@ -2,71 +2,57 @@ import wave
 import sys
 import streamlit as st
 import time
+import encode
+import decode
 
 st.title("Audio Steganography.")
 st.markdown("Select an audio file to get started! 🎵")
 
-def encode(sound, data, name):
-	parameters = sound.getparams()
-	frame_bytes = bytearray(list(sound.readframes(sound.getnframes())))
-	encoding, data_bits = '', []
-	if len(data * 8 * 8) > len(frame_bytes):
-		print("Cannot encode...")
-	else:
-		extra_bytes = int((len(frame_bytes)-(len(data) * 8 * 8)) / 8)
-		data = data + extra_bytes *'_'
-		for i in data:
-			data_bit = bin(ord(i)).lstrip('08b').zfill(8)
-			encoding = encoding + data_bit
-		data_bits = list(map(int, encoding))
-		for i, data_bit in enumerate(data_bits):
-			frame_bytes[i] = (frame_bytes[i] & 254) | data_bit
-		frame_bytes = bytes(frame_bytes)
-		encoded_sound = wave.open(f'encoded_{name}', 'wb')
-		encoded_sound.setparams(parameters)
-		encoded_sound.writeframes(frame_bytes)
-		st.markdown(f"Successfully hid your message! File saved as `encoded_{name}`")
-		encoded_sound.close()
-
-def decode(sound):
-	parameters = sound.getparams()
-	frame_bytes = bytearray(list(sound.readframes(sound.getnframes())))
-	extracted_bits, data = '', ''
-	for i in range(len(frame_bytes)):
-		extracted_bits = extracted_bits + str(frame_bytes[i] & 1)
-	for i in range(0, len(extracted_bits), 8):
-		byte = extracted_bits[i : i + 8]
-		character = chr(int(byte, 2))
-		data = data + character
-	data = data.split("___")[0]
-	return data
-
 if __name__=="__main__": 
+	
 	steg_option = st.radio("Choose steganography option: ", ("Encode", "Decode"))
 	if steg_option == "Encode":
-		encode_audio = st.file_uploader("Choose an audio file to encode with secret message:")
+		encode_audio = st.file_uploader("Choose an audio file to encode with secret message:", type = ['wav'])
 		if encode_audio:
 			file_name = encode_audio.name
-			encode_audio = wave.open(encode_audio, mode ='rb')
-			encode_option = st.radio("Choose data to be encoded: ", ("Type manually", "Upload file"))
-			if encode_option == "Type manually":
-				encode_text = st.text_area("Type something...")
-			elif encode_option == "Upload file":
-				try:
-					encode_text = st.file_uploader("Choose a text file containing the secret message:")
-					encode_text = str(encode_text.read())
-				except:
-					pass
-			if encode_text:
-				encode(encode_audio, encode_text, file_name)
-				encode_audio.close()
+			try:
+				encode_audio = wave.open(encode_audio, mode ='rb')
+				encode_option = st.radio("Choose data to be encoded: ", ("Type manually", "Upload file"))
+				
+				if encode_option == "Type manually":
+					encode_text = st.text_area("Type something...")
+				
+				elif encode_option == "Upload file":
+					try:
+						encode_text = st.file_uploader("Choose a text file containing the secret message:", type = ['txt'])
+						encode_text = str(encode_text.read())
+					except:
+						pass
+				if encode_text:
+					status = encode.encode(encode_audio, encode_text, file_name)
+					if status:
+						st.markdown(f"Successfully hid your message! File saved as `encoded_{file_name}`")
+					else:
+						st.markdown(f"Cannot encode. Text size exceeds file size.")
+					encode_audio.close()
+			except:
+				st.markdown(f"Oops!! Something went wrong.")
+				st.markdown(f"Please try again later.")
+			
+	
 	elif steg_option == "Decode":
-		decode_audio = st.file_uploader("Choose an audio file to be decoded:")
-		if decode_audio:
-			file_name = decode_audio.name
-			decode_audio = wave.open(decode_audio, mode ='rb')
-			decoded_text = decode(decode_audio)
-			decode_audio.close()
-			if decoded_text:
-				st.subheader("Sucessfully retrieved the hidden message: ")
-				st.markdown(str(decoded_text))
+		
+		decode_audio = st.file_uploader("Choose an audio file to be decoded:", type = ['wav'])
+		try:
+			if decode_audio:
+				file_name = decode_audio.name
+				decode_audio = wave.open(decode_audio, mode ='rb')
+				decoded_text = decode.decode(decode_audio)
+				decode_audio.close()
+				
+				if decoded_text:
+					st.subheader("Sucessfully retrieved the hidden message: ")
+					st.markdown(str(decoded_text))
+		except:
+				st.markdown(f"Oops!! Something went wrong.")
+				st.markdown(f"Please try again later.")
--- encode.py
+++ encode.py
@@ -1,167 +1,24 @@
-'''
-import wave , getopt , sys
-from struct import  *
-
-def preprocess(wav_file):
-    global sound , numberOfFrames , parameter , format , mask
-    sound = wave.open(wav_file, "r")
-    parameter = sound.getparams()
-    numberOfChannels = sound.getnchannels()
-    numberOfFrames = sound.getnframes()  # to how many pieces audio file is splat
-    sampleWidth = sound.getsampwidth()
-    numberOfSamples = numberOfFrames * numberOfChannels
-
-    if (sampleWidth == 1):
-        format = "{}B".format(numberOfSamples)  # changes into string
-        mask = 252
-    elif (sampleWidth == 2):
-        format = "{}h".format(numberOfSamples)  # format is to tell me how i ll unpack audio file
-        mask = 32764
-    else:
-        raise ValueError("Audio width is too high")
-
-    #print(sound, numberOfChannels, numberOfFrames,mask, format)
-
-
-def hide(wavName, text, resultFile):
-    preprocess(wavName)
-    maxByteToHide = (numberOfFrames * 2) // 8  # max bytes to hide
-
-    rawData = list(unpack(format, sound.readframes(numberOfFrames)))  # frames in binary
-    #rawData = bytearray(list(sound.readframes(sound.getnframes())))
-    sound.close()
-    textRawData = memoryview(open(text, "rb").read())  ## read text into tytes
-    print(rawData[0:10])
-    
-    firstVaues = []
-    indexOfText = 0
-    buferLen = 0
-    bufer = 0
-    soundindex = 0
-
-    while (indexOfText < len(textRawData)):  # incrementing lists index of the text
-
-        while (buferLen < 2):  # every time bufer len drops below 2 move to the next letter
-            bufer = textRawData[indexOfText]
-            indexOfText += 1
-            buferLen += 8
-
-        currentData = bufer % (1 << 2)  # bytes from text file extracted backwards for each letter
-        bufer >>= 2
-        buferLen -= 2
-
-        currentSample = rawData[soundindex]
-        soundindex += 1
-
-        sign = 1
-        if (currentSample < 0):
-            currentSample = - currentSample
-            sign = -1
-
-        changedSample = sign * (currentSample & mask) | currentData
-
-        tempValue = pack(format[-1], changedSample)
-        firstVaues.append(tempValue)
-
-    while (indexOfText < len(rawData)):
-        tempValue = pack(format[-1], rawData[indexOfText])
-        firstVaues.append(tempValue)
-        indexOfText += 1
-
-    resultAudio = wave.open(resultFile, "w")
-    resultAudio.setparams(parameter)
-    resultAudio.writeframesraw(b"".join(firstVaues))
-    resultAudio.close()
-    print("data hidden")
-
-hide('music.wav', 'data.txt', 'new.wav')
-'''
-'''
-import wave
-# read wave audio file
-song = wave.open("music.wav", mode='rb')
-# Read frames and convert to byte array
-frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-# The "secret" text message
-string='Peter Parker is the Spiderman!'
-# Append dummy data to fill out rest of the bytes. Receiver shall detect and remove these characters.
-string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
-# Convert text to bit array
-bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
-print(bits)
-
-# Replace LSB of each byte of the audio data by one bit from the text bit array
-for i, bit in enumerate(bits):
-    frame_bytes[i] = (frame_bytes[i] & 254) | bit
-# Get the modified bytes
-frame_modified = bytes(frame_bytes)
-
-# Write bytes to a new wave audio file
-with wave.open('song_embedded.wav', 'wb') as fd:
-    fd.setparams(song.getparams())
-    fd.writeframes(frame_modified)
-song.close()
-'''
-
-'''
-import wave
-song = wave.open("encoded.wav", mode='rb')
-# Convert audio to byte array
-frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-# Extract the LSB of each byte
-extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
-# Convert byte array back to string
-string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
-# Cut off at the filler characters
-decoded = string.split("___")[0]
-
-# Print the extracted text
-print("Sucessfully decoded: "+decoded)
-song.close()
-
-'''
-
 import wave
 import sys
 
-def encode(sound, data, parameters):
+def encode(sound, data, name):
+    parameters = sound.getparams()
     frame_bytes = bytearray(list(sound.readframes(sound.getnframes())))
-    encoding, data_bits = '', []
+    encoding, data_bits, status = '', [], True
     if len(data * 8 * 8) > len(frame_bytes):
-        print("Cannot encode....")
+        status = False
     else:
         extra_bytes = int((len(frame_bytes)-(len(data) * 8 * 8)) / 8)
         data = data + extra_bytes *'_'
-
         for i in data:
             data_bit = bin(ord(i)).lstrip('08b').zfill(8)
             encoding = encoding + data_bit
-        
         data_bits = list(map(int, encoding))
-      
         for i, data_bit in enumerate(data_bits):
             frame_bytes[i] = (frame_bytes[i] & 254) | data_bit
-        
         frame_bytes = bytes(frame_bytes)
-
-        encoded_sound = wave.open('encoded.wav', 'wb')
+        encoded_sound = wave.open(f'encoded_{name}', 'wb')
         encoded_sound.setparams(parameters)
         encoded_sound.writeframes(frame_bytes)
         encoded_sound.close()
-
-        print("Encoding completed successfully")
-        
-
-
-if __name__=="__main__": 
-    file = open('data.txt', 'r') 
-    data = file.read()
-    file.close()
-
-    sound = wave.open("music.wav", mode='rb')
-    parameters = sound.getparams()
-    encode(sound, data, parameters)
-    sound.close()
-    
+    return status
