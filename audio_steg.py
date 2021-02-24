20e44ed8dc58552eed202c0e526f6bb8b747d400
rubyruins <soumya.parekh@somaiya.edu>
2021-02-24 16:47:54 +0530

    added sidebar

--- audio_steg.py
+++ audio_steg.py
@@ -9,7 +9,13 @@ st.title("Audio Steganography.")
 st.markdown("Select an audio file to get started! 🎵")
 
 if __name__=="__main__": 
-	
+
+	st.sidebar.subheader("Algorithm used: LSB")
+	st.sidebar.markdown("This algorithm replaces the LSB (Least Significant Bit) of each byte in the _carrier_ data with one bit from the _secret_ message.")
+	st.sidebar.markdown("The sender performs _embedding_ of the bits of secret messages onto the carrier data byte-by-byte. The receiver performs the _extraction_ procedure by reading LSB bits of each byte of received data.")
+	st.sidebar.markdown("[![](https://img.shields.io/badge/Made_with-Python3-blue?style=for-the-badge&logo=python)]() [![](https://img.shields.io/badge/Made_with-streamlit-red?style=for-the-badge&logo=streamlit)]()")
+	st.sidebar.markdown("Made by: [Soumya Parekh (1811106)](https://github.com/rubyruins) and [Riya Tasgaonkar (1811122)](https://github.com/RiyaTasgaonkar)")
+
 	steg_option = st.radio("Choose steganography option: ", ("Encode", "Decode"))
 	if steg_option == "Encode":
 		encode_audio = st.file_uploader("Choose an audio file to encode with secret message:", type = ['wav'])
@@ -18,7 +24,7 @@ if __name__=="__main__":
 			try:
 				encode_audio = wave.open(encode_audio, mode ='rb')
 				encode_option = st.radio("Choose data to be encoded: ", ("Type manually", "Upload file"))
-				
+
 				if encode_option == "Type manually":
 					encode_text = st.text_area("Type something...")
 				
@@ -38,8 +44,7 @@ if __name__=="__main__":
 			except:
 				st.markdown(f"Oops!! Something went wrong.")
 				st.markdown(f"Please try again later.")
-			
-	
+
 	elif steg_option == "Decode":
 		
 		decode_audio = st.file_uploader("Choose an audio file to be decoded:", type = ['wav'])
